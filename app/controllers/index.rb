7237ac861cf246639e2f5f489d0a47711a1e31ba
Lukas Jones <joneslukas21@gmail.com>
2015-05-31 09:00:13 -0700

    added httparty and made route to home page from sign in work

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,1 +1,19 @@
-index.rb
+get "/" do
+	erb :index
+end
+
+put "/sign_in" do
+
+	options = params
+	p params
+	#@response = HTTParty.put('http://localhost:3000/api/v1/log_in', options)
+	response = HTTParty.put("http://localhost:3000/api/v1/log_in?email=#{params[:email]}&password_hash=#{params[:password_hash]}")
+
+	if response.body
+		@user = JSON.parse(response.body)
+		p @user
+		erb :main
+	else
+		erb :index
+	end
+end
--- public/js/signInUp.js
+++ public/js/signInUp.js
@@ -8,25 +8,25 @@ $(document).ready(function(){
 	$(".signUp").hide();
 
 
-	$(".signIn").on("submit", function(event){
-		event.preventDefault();
-		var formData = $(this).serialize();
-		// console.log($(this).serialize());
-		$.ajax({
-			url: "http://localhost:3000/api/v1/log_in",
-			type: "put",
-			data: formData,
-		})
-		.done(function(response){
-			console.log("sign in success");
-			console.log(response);
-			$("body").html("");
+	// $(".signIn").on("submit", function(event){
+	// 	event.preventDefault();
+	// 	var formData = $(this).serialize();
+	// 	// console.log($(this).serialize());
+	// 	$.ajax({
+	// 		url: "/sign_in",
+	// 		type: "get",
+	// 		data: formData,
+	// 	})
+	// 	.done(function(response){
+	// 		console.log("sign in success");
+	// 		console.log(response);
 			
-		})
-		.fail(function(){
-			console.log("sign in fail!");
-		})
-	})
+			
+	// 	})
+	// 	.fail(function(){
+	// 		console.log("sign in fail!");
+	// 	})
+	// })
 
 
 	$(".sign-form-swap").on("click", function(event){
