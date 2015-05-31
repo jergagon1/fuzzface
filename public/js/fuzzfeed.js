17fbaf2223977b3ef20edbe80caf9b905b633a1f
Sarah Kwak <sarahkwak@hotmail.com>
2015-05-31 14:25:16 -0700

    Handlebar downloaded and implemented in fuzzfeed_erb file. Fuzzfeed_JS file reday on port 3002.

--- public/js/fuzzfeed.js
+++ public/js/fuzzfeed.js
@@ -1,7 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $.ajax({
+   url: 'http://localhost:3002/articles',
+   dataType: 'JSON'
+  }).done(function(response){
+    var raw_template =  $('#article-template').html();
+    var template = Handlebars.compile(raw_template);
+        for (var i=0; i<response.length; i++) {
+    $('#articles').append(template(response[i]))
+  }
+  }).fail(function(response){
+    console.log("Something went wrong")
+  })
 
-  console.log("FuzzFeed JS loaded!")
+  $('#articles').on('click', 'a', function(evt) {
+    evt.preventDefault();
+    $.ajax({
+      url: 'http://localhost:3002/articles',
+      type: 'GET'
+    })
+    .done(function(response) {
+      var raw_template =  $('#comment-template').html();
+      var template = Handlebars.compile(raw_template);
+        for (var i=0; i<response.length; i++) {
+          $('.article_'+response[i].id).append(template(response[i]))
+        }
+    })
+    .fail(function() {
+      console.log("error");
+    })
+  })
 
+  $('#new_article').on('submit', function(evt) {
+    evt.preventDefault();
+    $.ajax({
+      url: 'http://localhost:3002/articles',
+      type: 'POST',
+      dataType: 'JSON',
+      data: $('#new_article').serialize()
+    })
+    .done(function(response) {
+      var raw_template =  $('#entry-template').html();
+      var template = Handlebars.compile(raw_template);
+      $('#articles').append(template(response));
+    })
+    .fail(function() {
+      console.log("error");
+    })
+  })
 
-
-});
+}) //ready
