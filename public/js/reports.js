d37d20812e7c302526ee4dfadbbdd43c1d209c48
Lukas Jones <joneslukas21@gmail.com>
2015-06-03 16:48:56 -0700

    finished found submit form.  reports js needs to be refactored

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,8 +1,10 @@
+
+
 $(document).ready(function(){
 	console.log("hello");
 	$(".lost-pet-form").on("submit", function(event){
 		event.preventDefault();
-		formData = $(this).serialize();
+		var formData = $(this).serialize();
 		console.log(formData);
 		$.ajax({
 			url: "http://localhost:3001/api/v1/reports",
@@ -12,12 +14,38 @@ $(document).ready(function(){
 		})
 		.done(function(response){
 			console.log(response);
+			$("input[type='text']").val('');
+			$("textarea").val("");
+			$("select").prop("selectedIndex", 0);
+			$(".main-buttons").siblings().first().slideUp("slow");
 		})
 		.fail(function(){
 			console.log('fail');
 		})
 
 
+	});
+
+	$(".found-pet-form").on("submit", function(event){
+		event.preventDefault();
+		var formData = $(this).serialize();
+		console.log(formData);
+		$.ajax({
+			url: "http://localhost:3001/api/v1/reports",
+			type: "post",
+			dataType: "json",
+			data: formData
+		})
+		.done(function(response){
+			console.log(response);
+			$("input[type='text']").val('');
+			$("textarea").val("");
+			$("select").prop("selectedIndex", 0);
+			$(".main-buttons").siblings().first().slideUp("slow");
+		})
+		.fail(function(){
+			console.log('fail');
+		})
 	})
 
 });
