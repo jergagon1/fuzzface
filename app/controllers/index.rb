4dd4d68dbc7789cadd86eb72d8148f085b0830f5
Jack <xjlin0@gmail.com>
2015-07-18 10:44:23 -0700

    fix S3 directly img upload in browser by changing bucket name

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,13 +3,13 @@ enable :sessions
 
 
 get "/jack" do
-	puts "Jack's testing"
   @page_title = "Jack's Testing"
+  puts "Jack's testing"
 	p @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
 	puts "Testing dotENV:"
 	p ENV['AWS_SECRET_ACCESS_KEY']
 	puts "Test bucket:"
-	p AWS::S3.new.buckets[ENV['S3_BUCKET']]
+	p AWS::S3.new.buckets[ENV['S3_BUCKET_NAME']]
 end
 
 get "/" do
@@ -22,7 +22,7 @@ get "/" do
     p gon.s3_hash = s3_hash
     @page_title = "FuzzFinders"
     @user_id = session[:user]["id"]
-    
+
 		erb :index
 	else
 		redirect "/sign_in"
@@ -38,7 +38,7 @@ put "/sign_in" do
   gon.s3_hash = s3_hash
 	options = params
 	response = HTTParty.put("http://localhost:3000/api/v1/log_in?email=#{params[:email]}&password_hash=#{params[:password_hash]}")
-	if response.body 
+	if response.body
 		@user = JSON.parse(response.body)
 		session[:user] = @user
 		redirect "/"
