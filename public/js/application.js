4f636d26de4db158f36bbff759821564640f4eab
Jeremy Gagon <jergagon1@gmail.com>
2015-08-04 11:16:57 -0700

    Add comments for documentation and adjust formatting

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
 
-  // menu button configuration
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
+  // Pages: All
+  // Toggle display of hamburger side menu
+  $("#menu-toggle").click(function(event) {
+    event.preventDefault();
     $("#wrapper").toggleClass("toggled");
   });
 
+  // Pages: FuzzFinders
+  // Hide/collapse the lost or found forms or the reports list if open on page load
   $(".main-buttons").siblings().hide();
-  // debugger
-  $(".main-buttons").on("click", function(e){
-    e.preventDefault();
+
+  // Pages: FuzzFinders, FuzzFeed
+  // Add event listener for large buttons to show or hide form and list content on click
+  $(".main-buttons").on("click", function(event){
+    event.preventDefault();
     if ($(this).siblings().first().is(":hidden")){
       $(this).siblings().first().slideDown("slow");
       $(this).addClass("selected-button");
