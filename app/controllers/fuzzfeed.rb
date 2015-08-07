5074a5a781e26f389af6e86b644065adb25b6c13
Jeremy Gagon <jergagon1@gmail.com>
2015-08-07 08:59:11 -0700

    Remove extra p statements and test route in sinatra controller

--- app/controllers/fuzzfeed.rb
+++ app/controllers/fuzzfeed.rb
@@ -2,8 +2,8 @@ get "/fuzzfeed" do
   if session[:user]
     @page_title = "FuzzFeed"
     @user_id = session[:user]["id"]
-    p session[:user]["email"]
-    p session[:user]["password_hash"]
+    # p session[:user]["email"]
+    # p session[:user]["password_hash"]
     wags_data = HTTParty.get("http://localhost:3000/api/v1/wags?email=#{session[:user]["email"]}&password_hash=#{session[:user]["password_hash"]}")
     @wags = JSON.parse(wags_data.body)["wags"]
     erb :fuzzfeed
