879f34712b8540ddc757040bb690c4a85524af0a
Jeremy Gagon <jergagon1@gmail.com>
2015-08-07 09:48:32 -0700

    Add s3 method to DRY up controller code

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,24 +12,20 @@ before do
   end
 end
 
-# get "/jack" do
-#   @page_title = "Jack's Testing"
-#   puts "Jack's testing"
-# 	p @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
-# 	puts "Testing dotENV:"
-# 	p ENV['AWS_SECRET_ACCESS_KEY']
-# 	puts "Test bucket:"
-# 	p AWS::S3.new.buckets[ENV['S3_BUCKET_NAME']]
-# end
+# setup method for image file upload to Amazon S3 bucket
+def setup_s3
+  @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+  @s3_hash = Hash.new
+  @s3_hash['url'] = @s3_direct_post.url
+  @s3_hash['urlstring'] = @s3_direct_post.url.to_s
+  @s3_hash['fields'] = @s3_direct_post.fields
+  gon.s3_hash = @s3_hash
+end
 
+# FuzzFinders page
 get "/" do
 	if session[:user]
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
-    s3_hash = Hash.new
-    s3_hash['url'] = @s3_direct_post.url
-    s3_hash['urlstring'] = @s3_direct_post.url.to_s
-    s3_hash['fields'] = @s3_direct_post.fields
-    gon.s3_hash = s3_hash
+    setup_s3
     @page_title = "FuzzFinders"
     @user_id = session[:user]["id"]
     # p session[:user]["email"]
@@ -42,13 +38,9 @@ get "/" do
 	end
 end
 
+# log in
 put "/sign_in" do
-  @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
-  s3_hash = Hash.new
-  s3_hash['url'] = @s3_direct_post.url
-  s3_hash['urlstring'] = @s3_direct_post.url.to_s
-  s3_hash['fields'] = @s3_direct_post.fields
-  gon.s3_hash = s3_hash
+  setup_s3
   options = params
   response = HTTParty.put("http://localhost:3000/api/v1/log_in?email=#{params[:email]}&password_hash=#{Digest::SHA1.hexdigest(params[:password_hash])}")
   unless response.body.empty?
