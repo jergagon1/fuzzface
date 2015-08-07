e42bb98fbcfed4845a907e69ddb5c29455ff8e59
Jeremy Gagon <jergagon1@gmail.com>
2015-08-07 11:34:12 -0700

    Call helper method to retrieve wags in fuzzfeed controller

--- app/controllers/fuzzfeed.rb
+++ app/controllers/fuzzfeed.rb
@@ -4,8 +4,7 @@ get "/fuzzfeed" do
     @user_id = session[:user]["id"]
     # p session[:user]["email"]
     # p session[:user]["password_hash"]
-    wags_data = HTTParty.get("http://localhost:3000/api/v1/wags?email=#{session[:user]["email"]}&password_hash=#{session[:user]["password_hash"]}")
-    @wags = JSON.parse(wags_data.body)["wags"]
+    retrieve_wags
     erb :fuzzfeed
   else
     redirect "/sign_in"
