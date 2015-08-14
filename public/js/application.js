d282004064f5264685c2830bfbc204e6aab9f5a6
Jeremy Gagon <jergagon1@gmail.com>
2015-08-14 11:06:44 -0700

    Move fuzzflash js to application.js

--- public/js/application.js
+++ public/js/application.js
@@ -16,4 +16,23 @@ $(document).ready(function() {
     });
   })();
 
+  // Pages: All
+  // Initialize FuzzFlash - Notification when new lost or found pet report created
+  var initializeFuzzFlash = (function(){
+    var clearFuzzflash = function() {
+      setTimeout(function() {
+        $('div.notification').text("");
+      }, 10000);
+    };
+
+    var pusher = new Pusher(gon.pusher_key);
+    var fuzzflashChannel = pusher.subscribe('fuzzflash');
+
+    fuzzflashChannel.bind('report_created', function(fuzzflash){
+      var message = fuzzflash.message;
+      $('div.notification').text(message);
+      clearFuzzflash();
+    });
+  })(); // IIFE initialFuzzFlash close
+
 });
