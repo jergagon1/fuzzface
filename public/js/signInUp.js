b241d76e08c4f166905f6823c38344cf995b7ecd
Jeremy Gagon <jergagon1@gmail.com>
2015-08-16 11:14:29 -0700

    Refactor signin signup js

--- public/js/signInUp.js
+++ public/js/signInUp.js
@@ -1,11 +1,44 @@
 $(function(){
 
-	$(".signUp").hide();
+  //------------------------- View -------------------------------//
 
-	$(".sign-form-swap").on("click", function(event){
-		event.preventDefault();
-		$(".signIn").toggle();
-		$(".signUp").toggle();
-	});
+  // View: hide the sign up form
+  var hideSignUpForm = function(){
+  	$(".signUp").hide();
+  };
+
+  // View: toggle the sign in and sign up forms
+  var toggleSignInSignUpForm = function(){
+    $(".signIn").toggle();
+    $(".signUp").toggle();
+  };
+
+
+  //------------------------- Controller -------------------------------//
+
+  // Controller: add event listener to link to toggle sign in and sign up form
+  var addEventListenerToggleSignInSignUpForm = function(){
+  	$(".sign-form-swap").on("click", function(event){
+  	  event.preventDefault();
+  	  toggleSignInSignUpForm();
+  	});
+  };
+
+  // Controller: remove event listener to link to toggle sign in and sign up form
+  var removeEventListenerToggleSignInSignUpForm = function(){
+    $(".sign-form-swap").off("click");
+  };
+
+  // Controller: initialize event handlers if on signin/signup page
+  var initializeSignInUp = (function(){
+    if (checkForElement(".sign-in-form-container")) {
+      // add initializers and event listeners
+      hideSignUpForm();
+      addEventListenerToggleSignInSignUpForm();
+    } else {
+      // remove initializers and event listeners
+      removeEventListenerToggleSignInSignUpForm();
+    }
+  })();
 
 }); // close document ready
